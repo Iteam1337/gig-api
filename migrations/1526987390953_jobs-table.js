b9f915d23ccc474286de36ef98cf33857716db00
Jimmy Jardland <jimmy@jardland.se>
2018-05-23 11:38:55 +0200

    jobs table

---
+++ migrations/1526987390953_jobs-table.js
@@ -0,0 +1,26 @@
+exports.up = pgm => {
+  pgm.sql('CREATE EXTENSION IF NOT EXISTS "uuid-ossp"')
+  pgm.createTable('jobs', {
+    id: { type: 'uuid', default: pgm.func('uuid_generate_v4()'), primaryKey: true, notNull: true },
+    type: { type: 'text', notNull: true },
+    company: { type: 'text', notNull: true },
+    title: { type: 'text', notNull: true },
+    preamble: { type: 'text', notNull: true },
+    text: { type: 'text', notNull: true },
+    created_at: { type: 'timestamp', notNull: true, default: pgm.func('NOW()') },
+    language: { type: 'text', notNull: true },
+    url: { type: 'text', notNull: true },
+    contact: { type: 'text', notNull: true },
+    fee: { type: 'jsonb', notNull: true },
+    categories: {type: 'jsonb', notNull: true },
+    start_date: { type: 'timestamp', notNull: true },
+    end_date: { type: 'timestamp', notNull: true },
+    listed_date: { type: 'timestamp', notNull: false },
+    source: { type: 'text', notNull: false },
+    entry_by: { type: 'text', notNull: false },
+  })
+}
+
+exports.down = pgm => {
+  pgm.dropTable('jobs')
+}
