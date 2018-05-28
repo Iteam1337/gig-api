cfe55c2c03a63c1650652140ca228c598de4ca8b
Dennis Pettersson <mail@dennispettersson.se>
2018-05-28 15:40:28 +0200

    should be drop~

--- migrations/1527507547810_radius.js
+++ migrations/1527507547810_radius.js
@@ -3,9 +3,11 @@ exports.shorthands = undefined;
 exports.up = pgm => {
   pgm.sql('CREATE EXTENSION IF NOT EXISTS "cube";')
   pgm.sql('CREATE EXTENSION IF NOT EXISTS "earthdistance";')
+  // pgm.sql('CREATE INDEX "latitude_longitude" on "jobs" USING gist(ll_to_earth("latitude", "longitude"));')
 }
 
 exports.down = pgm => {
-  pgm.sql('DELETE EXTENSION IF EXISTS "cube";')
-  pgm.sql('DELETE EXTENSION IF EXISTS "earthdistance";')
+  pgm.sql('DROP EXTENSION IF EXISTS "earthdistance" cascade;')
+  pgm.sql('DROP EXTENSION IF EXISTS "cube" cascade;')
+  // pgm.sql('DROP INDEX IF EXISTS "latitude_longitude";')
 }
